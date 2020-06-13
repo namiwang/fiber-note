47508154bef0c878d0d398e4f66d85fc85eed280
Nami.W <namiheike@gmail.com>
2020-06-13 15:41:27 +0800

    factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include FactoryBot::Syntax::Methods
 end
