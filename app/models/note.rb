37a88415687dad96a940e1eae7ab8558f8c78146
Nami.W <namiheike@gmail.com>
2020-06-13 15:48:15 +0800

    auto annotate

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :uuid             not null, primary key
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_notes_on_title  (title) UNIQUE
+#
 class Note < ApplicationRecord
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :uuid             not null, primary key
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_notes_on_title  (title) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
