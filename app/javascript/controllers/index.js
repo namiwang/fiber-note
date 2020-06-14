950edc824f461ea07b297cf766451f73a41715da
Nami.W <namiheike@gmail.com>
2020-06-14 15:23:05 +0800

    loading both js and ts files

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,5 +5,5 @@ import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
 const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
+const context = require.context("controllers", true, /_controller\.[jt]s$/)
 application.load(definitionsFromContext(context))
