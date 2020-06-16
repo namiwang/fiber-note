f9de6f272651577f93fe1e8bdcc99914851cda0f
Nami.W <namiheike@gmail.com>
2020-06-16 11:46:51 +0800

    WIP on testing prosemirror

--- app/javascript/controllers/note_controller.ts
+++ app/javascript/controllers/note_controller.ts
@@ -1,6 +1,11 @@
 import { Controller } from 'stimulus'
 
-import 'trix'
+import {EditorState} from "prosemirror-state"
+import {EditorView} from "prosemirror-view"
+import {Schema, DOMParser} from "prosemirror-model"
+import {schema} from "prosemirror-schema-basic"
+import {addListNodes} from "prosemirror-schema-list"
+import {exampleSetup} from "prosemirror-example-setup"
 
 class Note {
   constructor(
@@ -44,6 +49,24 @@ export default class extends Controller {
     this.note = new Note(
       this.data.get('id')
     )
+
+    this.initEditor()
+  }
+
+  private initEditor() {
+    // Mix the nodes from prosemirror-schema-list into the basic schema to
+    // create a schema with list support.
+    let mySchema = new Schema({
+      nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+      marks: schema.spec.marks
+    })
+
+    let view = new EditorView(this.editorTarget, {
+      state: EditorState.create({
+        doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
+        plugins: exampleSetup({schema: mySchema})
+      })
+    })
   }
 
   async updateTitle(event: Event) {
