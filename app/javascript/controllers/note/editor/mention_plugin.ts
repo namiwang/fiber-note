13f404f8ebd0ece4b1a4c598bfb55f226894931a
Nami.W <namiheike@gmail.com>
2020-06-20 16:27:16 +0800

    WIP mention_plugin: strip mentionning and leaves tagging

--- app/javascript/controllers/note/editor/mention_plugin.ts
+++ app/javascript/controllers/note/editor/mention_plugin.ts
@@ -1,38 +1,19 @@
 import {getMentionsPlugin} from './mention_plugin/mention_plugin'
 
-/**
- * IMPORTANT: outer div's "suggestion-item-list" class is mandatory. The plugin uses this class for querying.
- * IMPORTANT: inner div's "suggestion-item" class is mandatory too for the same reasons
- */
-var getMentionSuggestionsHTML = items => '<div class="suggestion-item-list">'+
-  items.map(i => '<div class="suggestion-item">'+i.name+'</div>').join('')+
-'</div>'
-
-/**
- * IMPORTANT: outer div's "suggestion-item-list" class is mandatory. The plugin uses this class for querying.
- * IMPORTANT: inner div's "suggestion-item" class is mandatory too for the same reasons
- */
-var getTagSuggestionsHTML = items => '<div class="suggestion-item-list">'+
-  items.map(i => '<div class="suggestion-item">'+i.tag+'</div>').join('')+
-'</div>'
-
 export const mentionPlugin = getMentionsPlugin({
   getSuggestions: (type, text, done) => {
     setTimeout(() => {
-      if (type === 'mention') {
-        // pass dummy mention suggestions
-        done([{name: 'John Doe', id: '101', email: 'joe@gmail.com'}, {name: 'Joe Lewis', id: '102', email: 'lewis@gmail.com'}])
-      } else {
-        // pass dummy tag suggestions
-        done([{tag: 'WikiLeaks'}, {tag: 'NetNeutrality'}])
-      }
+      // pass dummy tag suggestions
+      done([{tag: 'WikiLeaks'}, {tag: 'NetNeutrality'}])
     }, 0)
   },
   getSuggestionsHTML: (items, type) => {
-    if (type === 'mention') {
-      return getMentionSuggestionsHTML(items)
-    } else if (type === 'tag') {
-      return getTagSuggestionsHTML(items)
-    }
+    /**
+     * IMPORTANT: outer div's "suggestion-item-list" class is mandatory. The plugin uses this class for querying.
+     * IMPORTANT: inner div's "suggestion-item" class is mandatory too for the same reasons
+     */
+    return '<div class="suggestion-item-list">'+
+      items.map(i => '<div class="suggestion-item">'+i.tag+'</div>').join('')+
+    '</div>'
   }
 })
