a8a024c288e3472b84793da6bb3e1489e9400d61
Nami.W <namiheike@gmail.com>
2020-06-23 13:22:52 +0800

    init network graph

--- app/javascript/controllers/network_controller.ts
+++ app/javascript/controllers/network_controller.ts
@@ -1,14 +1,50 @@
 import { Controller } from 'stimulus'
 
+import cytoscape from 'cytoscape'
+
 export default class extends Controller {
   static targets = ['graph']
 
   // https://github.com/stimulusjs/stimulus/search?q=targets+typescript&type=Issues
-  graphTarget: Element
+  graphTarget: HTMLElement
 
   connect() {
     console.log('stimulus: network connected on:')
     console.log(this.element)
+
+    this.initGraph()
+  }
+
+  private initGraph() {
+    cytoscape({
+      container: this.graphTarget,
+      elements: {
+        nodes: [
+          {
+            data: { id: 'a' }
+          },
+          {
+            data: { id: 'b' }
+          }
+        ],
+        edges: [
+          {
+            data: { id: 'ab', source: 'a', target: 'b' }
+          }
+        ]
+      },
+      layout: {
+        name: 'preset'
+      },
+      style: [
+        {
+          selector: 'node',
+          style: {
+            'label': 'data(id)'
+          }
+        }
+      ]
+    })
   }
 
 }
