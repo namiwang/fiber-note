628c69ee6872376bea7071e19f15fc29ef8101fa
Nami.W <namiheike@gmail.com>
2020-07-06 19:53:28 +0800

    fixed editor for block

--- app/components/notes/editor_component.rb
+++ app/components/notes/editor_component.rb
@@ -1,20 +1,20 @@
 class Notes::EditorComponent < ViewComponent::Base
   def initialize block
-    mode = if block.is_note? then :note else :block end
+    note = if block.is_note? then block else block.root_note end
 
     @editor_data = {
-      # mode: mode,
       note: {
-        id: block.id,
-        title: block.title,
-        content: BlockSerializer.new(block).as_note_doc.to_json,
+        id: note.id,
+        title: note.title,
+        content: BlockSerializer.new(note).as_note_doc.to_json,
       },
       available_tags: Block.available_tags,
     }
 
-    if mode == :block
-      @editor_data[:hidden_block_ids] = block.child_block_ids - [block.id]
+    # in linked block
+    # TODO better naming
+    if block.is_note?
+      @editor_data[:hidden_block_ids] = note.child_block_ids - [note.id]
     end
-
   end
 end
