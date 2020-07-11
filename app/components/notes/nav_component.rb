8e910637ce3695799d78f10b66773cc9b1c91b55
Nami.W <namiheike@gmail.com>
2020-07-11 13:05:18 +0800

    removed @creating_new flag in notes/nav

--- app/components/notes/nav_component.rb
+++ app/components/notes/nav_component.rb
@@ -2,6 +2,5 @@ class Notes::NavComponent < ViewComponent::Base
   def initialize current_note
     @note_titles = Block.available_tags
     @current_note = current_note
-    @creating_new = !current_note.persisted?
   end
 end
