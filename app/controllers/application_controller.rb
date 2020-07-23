3724ae0008826a1986d6094b1f0d8fc0136b9540
Nami.W <namiheike@gmail.com>
2020-07-23 17:25:21 +0800

    enforce login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :enforce_login
+
+  private
+ 
+  def enforce_login
+    redirect_to new_session_path unless session[:session]
+  end
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,6 @@
 class SessionController < ApplicationController
+  skip_before_action :enforce_login, only: [:new, :create]
+
   def new
   end
 
