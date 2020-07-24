903a0782a2f8ff7b98cee79d30a37d894318665e
Nami.W <namiheike@gmail.com>
2020-07-24 09:18:25 +0800

    minor tweaks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@
 
 // require("@rails/ujs").start()
 // require("turbolinks").start()
-require("@rails/activestorage").start()
+// require("@rails/activestorage").start()
 require("channels")
 
 import "controllers"
